1d399820e089a3d1b5cb259ba20128f97c4538fe
JudithUru <judith.uruchima@uees.edu.ec>
2025-06-19 15:00:22 -0500

    extends del chatbot

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Miniso Manger</title>
+    <title>Miniso Manager</title>
 
     <script src="assets/js/color-modes.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
